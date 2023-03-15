c01442680e53a4b45eadfe586272cf7745c0aeba
Ha Le <114164123+hallie304@users.noreply.github.com>
2023-03-15 16:17:23 +0700

    Update dora-and-search.cpp

--- CodeForces/dora-and-search.cpp
+++ CodeForces/dora-and-search.cpp
@@ -1,26 +1,27 @@
+
 #include <iostream>
 #include <vector>
 using namespace std;
-
+ 
 int main() {
     int t;
     cin >> t;
-
+ 
     while (t--) {
         int n;
         cin >> n;
-
+ 
         vector<int> a(n + 1);
         for (int i = 1; i <= n; i++) {
             cin >> a[i];
         }
-
+ 
         int l = 1, r = n, L = 1, R = n;
         bool foundRange = false;
-
+ 
         while (l < r && !foundRange) {
             foundRange = true;
-
+ 
             if (a[l] == L) {
                 l++, L++;
             } else if (a[l] == R) {
@@ -33,13 +34,13 @@ int main() {
                 foundRange = false;
             }
         }
-
+ 
         if (foundRange) {
             cout << l << " " << r << endl;
         } else {
             cout << -1 << endl;
         }
     }
-
+ 
     return 0;
 }
