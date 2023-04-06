080c27a83640b2776dbe8c2d813188955e294669
hallie304 <lethihongha.work@gmail.com>
2023-04-06 21:06:38 +0700

    Add new file

--- CodeForces/Greg and Array.cpp
+++ CodeForces/Greg and Array.cpp
@@ -1,17 +1,15 @@
 #include <bits/stdc++.h>
 #define int long long
-const int N = 1e5+5;
-
 using namespace std;
-
+const int N = 1e5 + 5;
 int n, m, k;
-int a[N], c[N], l[N], r[N], d[N], x[N], y[N], cnt[N];
+int a[N], l[N], r[N], d[N], x[N], y[N], cnt[N], res[N];
 
 signed main(){
-    cin>>n>>m>>k;
+    cin >> n >> m >> k;
     for (int i = 1; i <= n; i++){
         cin >> a[i];
-        c[i] = a[i] - a[i-1];
+        res[i] = a[i] - a[i-1];
     }
     for (int i = 1; i <= m; i++) cin>>l[i]>>r[i]>>d[i];
 
@@ -23,13 +21,13 @@ signed main(){
     
     for (int i = 1; i <= m; i++){
         cnt[i] += cnt[i-1]; 
-        c[l[i]] += cnt[i] * d[i]; 
-        c[r[i]+1] -= cnt[i] * d[i]; 
+        res[l[i]] += cnt[i] * d[i]; 
+        res[r[i]+1] -= cnt[i] * d[i]; 
     }
     
     for (int i = 1; i <= n; i++){
-        c[i] += c[i-1]; 
-        cout << c[i] << ' '; 
+        res[i] += res[i-1]; 
+        cout << res[i] << ' '; 
     }
     return 0;
 }
